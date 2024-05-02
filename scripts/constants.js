f774a8b8db999ec17a058630335b89b1c6d3815a
Darin Kuntze <dkuntze@gmail.com>
2024-05-02 11:37:35 -0500

    hero

---
+++ scripts/constants.js
@@ -0,0 +1,28 @@
+// ONE TRUST COOKIE CONSENT
+export const DATA_DOMAIN_SCRIPT = 'e8ffed56-4bb1-43fd-9b41-bc4385267ec8';
+
+// GOOGLE TAG MANAGER ID
+export const GTM_ID = 'GTM-KP9KZWR';
+
+// HOTJAR ID
+export const HOTJAR_ID = 1139895;
+
+// COOKIE Constants
+export const COOKIE_VALUES = {
+  performance: 'C0002',
+  targeting: 'C0004',
+  social: 'C0005',
+};
+
+// ACCOUNT ENGAGEMENT TRACKING Constants
+export const ACCOUNT_ENGAGEMENT_TRACKING_CONSTANTS = {
+  piAId: 1039333,
+  piCId: 25825,
+  piHostname: 'pi.pardot.com',
+};
+
+// Search API URLs
+export const SEARCH_URLS = {
+  prod: 'https://kb3ko4nzt2.execute-api.eu-west-1.amazonaws.com/prod/search',
+  dev: 'https://search-api-dev.aws.43636.vnonprod.com/search',
+};
